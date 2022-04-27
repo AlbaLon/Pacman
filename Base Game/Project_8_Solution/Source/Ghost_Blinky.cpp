5755dce885678a71180f613c5f4ca8071e269d33
AlbaLon <99950307+AlbaLon@users.noreply.github.com>
2022-04-27 10:50:29 +0200

    Pequeños cambios

--- Base Game/Project_8_Solution/Source/Ghost_Blinky.cpp
+++ Base Game/Project_8_Solution/Source/Ghost_Blinky.cpp
@@ -5,22 +5,26 @@
 
 Enemy_Blinky :: Enemy_Blinky(int x, int y) : Enemy(x, y)
 {//TODO DEFINIRLO EN LOS PUSHBACKS Y ANIMATION Y PATH O LO QUE SEA
-	up.PushBack({ 5, 108, 31, 29 });
-	up.PushBack({ 4, 141, 31, 29 });
-	up.PushBack({ 38, 108, 31, 29 });
+	up.PushBack({ 161, 111, 31, 29 });
+	up.PushBack({ 161, 105, 31, 29 });
+	up.PushBack({ 161, 111, 31, 29 });
 	up.speed = 0.1f;
+	up.loop = true;
 	up.pingpong = true; //Esto hace que la animación vaya de adelante a atras y atras adelante, si la animacion es ciclica hay que poner el loop
 
-	down.PushBack({ 170, 108, 31, 29 });
-	down.PushBack({ 170, 141, 31, 29 });
-	down.PushBack({ 137, 108, 31, 29 });
+	down.PushBack({ 65, 111, 31, 29 });
+	down.PushBack({ 65, 105, 31, 29 });
+	down.PushBack({ 65, 111, 31, 29 });
+	down.loop = true;
 	down.speed = 0.1f;
+
 	
 	//TODO: PROGRAMAR EL MOVIMIENTO DEL FANTASMA, EN ESTE CASO ES UNA IA SUPER BASICA
 	/*path.PushBack({ -0.3f, 0.0f }, 150, &front);
 	path.PushBack({ 1.2f, 0.0f }, 150, &back);*/
 
 	collider = App->collisions->AddCollider({ 0, 0, 24, 24 }, Collider::Type::ENEMY, (Module*)App->enemies);
+
 }
 
 void Enemy_Blinky::Update()
--- Base Game/Project_8_Solution/Source/Ghost_Inky.cpp
+++ Base Game/Project_8_Solution/Source/Ghost_Inky.cpp
@@ -7,15 +7,17 @@
 
 Enemy_Inky::Enemy_Inky(int x, int y) : Enemy(x, y)
 {//TODO DEFINIRLO EN LOS PUSHBACKS Y ANIMATION Y PATH O LO QUE SEA
-	up.PushBack({ 5, 108, 31, 29 });
-	up.PushBack({ 4, 141, 31, 29 });
-	up.PushBack({ 38, 108, 31, 29 });
+	up.PushBack({ 163, 103, 31, 29 });
+	up.PushBack({ 163, 99, 31, 29 });
+	up.PushBack({ 163, 103, 31, 29 });
 	up.speed = 0.1f;
+	up.loop = true;
 	up.pingpong = true; //Esto hace que la animación vaya de adelante a atras y atras adelante, si la animacion es ciclica hay que poner el loop
 
-	down.PushBack({ 170, 108, 31, 29 });
-	down.PushBack({ 170, 141, 31, 29 });
-	down.PushBack({ 137, 108, 31, 29 });
+	down.PushBack({ 63, 103, 31, 29 });
+	down.PushBack({ 63, 99, 31, 29 });
+	down.PushBack({ 63, 103, 31, 29 });
+	down.loop = true;
 	down.speed = 0.1f;
 
 	//TODO: PROGRAMAR EL MOVIMIENTO DEL FANTASMA, EN ESTE CASO ES UNA IA SUPER BASICA

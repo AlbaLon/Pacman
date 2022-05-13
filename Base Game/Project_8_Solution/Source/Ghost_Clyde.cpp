3cd24dc005db6d1bd8496cc66e76a2b7233ac9fb
AlbaLon <99950307+AlbaLon@users.noreply.github.com>
2022-05-13 09:00:47 +0200

    Supuesta animación Clyde

--- Base Game/Project_8_Solution/Source/Ghost_Clyde.cpp
+++ Base Game/Project_8_Solution/Source/Ghost_Clyde.cpp
@@ -7,56 +7,29 @@ Enemy_Clyde::Enemy_Clyde(int x, int y) : Enemy(x, y)
 {//TODO: CUADRAR LA ANIMACION A LA SPRITE SHEET
 
 	//ALBA: ANIMAR
-	up.PushBack({ 162, 111, 16, 17 });
-	up.PushBack({ 162, 111, 16, 18 });
-	up.PushBack({ 162, 111, 16, 19 });
-	up.PushBack({ 162, 111, 16, 20 });
-	up.PushBack({ 162, 105, 16, 21 });
-	up.PushBack({ 162, 111, 16, 20 });
-	up.PushBack({ 162, 111, 16, 19 });
-	up.PushBack({ 162, 111, 16, 18 });
-	up.PushBack({ 162, 111, 16, 17 });
+	up.PushBack({ 189, 79, 16, 17 });
+	up.PushBack({ 189, 76, 16, 17 });
+	up.PushBack({ 189, 79, 16, 17 });
 	up.speed = 0.1f;
 	up.loop = true;
 	up.pingpong = true; //Esto hace que la animación vaya de adelante a atras y atras adelante, si la animacion es ciclica hay que poner el loop
 
-	down.PushBack({ 66, 111, 16, 17 });
-	down.PushBack({ 66, 111, 16, 18 });
-	down.PushBack({ 66, 111, 16, 19 });
-	down.PushBack({ 66, 105, 16, 20 });
-	down.PushBack({ 66, 105, 16, 21 });
-	down.PushBack({ 66, 105, 16, 21 });
-	down.PushBack({ 66, 105, 16, 20 });
-	down.PushBack({ 66, 111, 16, 19 });
-	down.PushBack({ 66, 111, 16, 18 });
-	down.PushBack({ 66, 111, 16, 17 });
+	down.PushBack({ 93, 79, 16, 17 });
+	down.PushBack({ 93, 76, 16, 18 });
+	down.PushBack({ 93, 79, 16, 17 });
 	down.loop = true;
 	down.speed = 0.1f;
 
-	rigth.PushBack({ 114, 111, 16, 17 });
-	rigth.PushBack({ 114, 111, 16, 18 });
-	rigth.PushBack({ 114, 111, 16, 19 });
-	rigth.PushBack({ 114, 111, 16, 20 });
-	rigth.PushBack({ 114, 105, 16, 21 });
-	rigth.PushBack({ 114, 105, 16, 21 });
-	rigth.PushBack({ 114, 111, 16, 20 });
-	rigth.PushBack({ 114, 111, 16, 19 });
-	rigth.PushBack({ 114, 111, 16, 18 });
-	rigth.PushBack({ 114, 111, 16, 17 });
+	rigth.PushBack({ 141, 79, 16, 17 });
+	rigth.PushBack({ 141, 76, 16, 17 });
+	rigth.PushBack({ 141, 79, 16, 17 });
 	//rigth.pingpong = true;
 	rigth.loop = true;
 	rigth.speed = 0.1f;
 
-	left.PushBack({ 18, 111, 16, 17 });
-	left.PushBack({ 18, 111, 16, 18 });
-	left.PushBack({ 18, 111, 16, 19 });
-	left.PushBack({ 18, 111, 16, 20 });
-	left.PushBack({ 18, 105, 16, 21 });
-	left.PushBack({ 18, 105, 16, 21 });
-	left.PushBack({ 18, 111, 16, 20 });
-	left.PushBack({ 18, 111, 16, 19 });
-	left.PushBack({ 18, 111, 16, 18 });
-	left.PushBack({ 18, 111, 16, 17 });
+	left.PushBack({ 45, 79, 16, 17 });
+	left.PushBack({ 45, 76, 16, 17 });
+	left.PushBack({ 45, 79, 16, 17 });
 	left.loop = true;
 	left.speed = 0.1f;
 

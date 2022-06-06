0ecb97c2184263996969e54cc332756976bcfb59
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2022-06-06 22:46:12 +0200

    Update ModuleGameOver.cpp

--- Base Game/Project_8_Solution/Source/ModuleGameOver.cpp
+++ Base Game/Project_8_Solution/Source/ModuleGameOver.cpp
@@ -10,7 +10,29 @@
 
 SceneGameOver::SceneGameOver(bool startEnabled) : Module(startEnabled)
 {
-
+	bgGOver.PushBack({ 0, 0, 232, 304 });
+	bgGOver.PushBack({ 233, 0, 232, 304 });
+	bgGOver.PushBack({ 465, 0, 232, 304 });
+	bgGOver.PushBack({ 697, 0, 232, 304 });
+	bgGOver.PushBack({ 929, 0, 232, 304 });
+	bgGOver.PushBack({ 1161, 0, 232, 304 });
+	bgGOver.PushBack({ 1393, 0, 232, 304 });
+	bgGOver.PushBack({ 1625, 0, 232, 304 });
+	bgGOver.PushBack({ 1857, 0, 232, 304 });
+	bgGOver.PushBack({ 2088, 0, 232, 304 });
+	bgGOver.PushBack({ 0, 305, 232, 304 });
+	bgGOver.PushBack({ 233, 305, 232, 304 });
+	bgGOver.PushBack({ 465, 305, 232, 304 });
+	bgGOver.PushBack({ 697, 305, 232, 304 });
+	bgGOver.PushBack({ 929, 305, 232, 304 });
+	bgGOver.PushBack({ 1161, 305, 232, 304 });
+	bgGOver.PushBack({ 1393, 305, 232, 304 });
+	bgGOver.PushBack({ 1625, 305, 232, 304 });
+	bgGOver.PushBack({ 1857, 305, 232, 304 });
+	bgGOver.PushBack({ 2088, 305, 232, 304 });
+	bgGOver.loop = false;
+	bgGOver.pingpong = false;
+	bgGOver.speed = 0.2f;
 }
 
 SceneGameOver::~SceneGameOver()
@@ -25,8 +47,10 @@ bool SceneGameOver::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/Sprites/lose2.png");
-	App->audio->PlayMusic("Assets/Music/Ending.ogg", 1.0f);
+	bgTexture = App->textures->Load("Assets/Sprites/Game Over Animated.png");
+	App->audio->PlayMusic("Assets/Music/gameover.ogg", 1.0f);
+
+	GameOverAnim = &bgGOver;
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -38,7 +62,8 @@ bool SceneGameOver::Start()
 
 Update_Status SceneGameOver::Update()
 {
-	}
+	GameOverAnim->Update();
+
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
 	{
 		App->fade->FadeToBlack(this, (Module*)App->sceneIntroGame, 90);
@@ -51,7 +76,8 @@ Update_Status SceneGameOver::Update()
 Update_Status SceneGameOver::PostUpdate()
 {
 	// Draw everything --------------------------------------
-	App->render->Blit(bgTexture, 0, 0, NULL);
+	SDL_Rect rectGOver = GameOverAnim->GetCurrentFrame();
+	App->render->Blit(bgTexture, 0, 0, &rectGOver);
 
 	return Update_Status::UPDATE_CONTINUE;
 }

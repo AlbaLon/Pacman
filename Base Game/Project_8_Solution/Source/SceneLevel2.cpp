35b375139506ab676eec58080eab08060d22e4fa
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2022-06-06 21:28:14 +0200

    Gameober song

--- Base Game/Project_8_Solution/Source/SceneLevel2.cpp
+++ Base Game/Project_8_Solution/Source/SceneLevel2.cpp
@@ -34,7 +34,7 @@ bool SceneLevel2::Start()
 	bool ret = true;
 
 	//bgTexture = App->textures->Load("Assets/Sprites/map.png"); //ERIC: CAMBIADO AL MAPA
-	bgTexture = App->textures->Load("Assets/Sprites/map2goodLineas.png"); //La grid para ver movimiento pac man  
+	bgTexture = App->textures->Load("Assets/Sprites/map2good.png"); //La grid para ver movimiento pac man  
 	int musicainicioFx = App->audio->LoadFx("Assets/Fx/Inicio_de_partida.wav");
 	App->audio->PlayFx(musicainicioFx, 0);
 	App->audio->PlayMusic("Assets/Music/musica_mundo_1.ogg", 1.0f);
